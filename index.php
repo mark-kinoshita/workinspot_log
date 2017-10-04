e1a4130900a9faed3597872662f5f675b1284c84
mkinoshita <mkinoshita@csumb.edu>
2017-10-04 04:18:42 +0000

    first push

---
+++ index.php
@@ -0,0 +1,93 @@
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+table, th, td {
+    border: 1px solid black;
+    padding: 5px;
+}
+table {
+    border-spacing: 15px;
+}
+</style>
+</head>
+<body>
+  <?php
+  ?>
+
+<table style="width:100%">
+  <h1>October</h1>
+  <tr>
+    <th>Date</th>
+    <th>Topic</th> 
+    <th>Hours worked</th>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</table>
+
+<table style="width:100%">
+  <h1>November</h1>
+  <tr>
+    <th>Date</th>
+    <th>Topic</th> 
+    <th>Hours worked</th>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</table>
+
+<table style="width:100%">
+  <h1>December</h1>
+  <tr>
+    <th>Date</th>
+    <th>Topic</th> 
+    <th>Hours worked</th>
+  </tr>
+  <tr>
+    <td>10/4/17</td>
+    <td>User registration</td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td></td>
+  </tr>
+</table>
+
+
+
+</body>
+</html>
